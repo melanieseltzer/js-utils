2dd76177bc605ab456f10cba5c0c034d4bb8bb38
Melanie Seltzer <melleh11@gmail.com>
2018-10-28 21:45:45 -0700

    Modify renameFile

--- utils.js
+++ utils.js
@@ -1,9 +1,14 @@
-// Rename file to strip spaces and use Date.now method for timestamp
-export const renameFile = filename => {
-  const baseName = filename.substring(0, filename.lastIndexOf('.'));
+// Takes in a filename, replaces spaces with dashes, and
+// returns a new name with Date.now() for timestamp
+export function renameFile(filename) {
+  // Get the base name without the extension
+  const baseName = filename.replace(/\s+/g, '-').substring(0, filename.lastIndexOf('.'));
+
+  // Get the file extension
   const ext = filename.substring(filename.lastIndexOf('.') + 1, filename.length);
 
-  return `${baseName}-${Date.now()}.${ext}`.replace(/\s+/g, '-');
+  // Return the new name as a string
+  return `${baseName}-${Date.now()}.${ext}`;
 }
 
 // Wait for process to exit before moving on to the next task
